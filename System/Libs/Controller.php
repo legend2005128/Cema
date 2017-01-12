c7b976a7a30bc0878ce1003c7bb073a6f3be0040
li <hao2005128@126.com>
2017-01-12 17:25:18 +0800

    1111

---
+++ System/Libs/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 2017/1/12
+ * Time: 9:48
+ */
+
+namespace System\Libs;
+
+
+class Controller
+{
+
+}
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 2017/1/11
+ * Time: 15:53
+ */
+if (version_compare(PHP_VERSION, '5.3', '<'))
+{
+    die("phpversion require '5.3+''");
+}
+//定义debug
+define('APP_DEBUG',TRUE);
+if(APP_DEBUG){
+    ini_set('diaplay_errors',0);
+    if (version_compare(PHP_VERSION, '5.3', '>='))
+    {
+        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+    }
+    else
+    {
+        error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+    }
+}else{
+    ini_set('diaplay_errors',false);
+}
+
+//定义根目录
+define('WEB_PATH',__DIR__);
+//系统类库路径
+define('SYS_PATH',WEB_PATH.'/'.'System');
+//业务逻辑方法库
+define('APP_PATH',WEB_PATH.'/'.'App');
+
+//加载类
+
+require_once SYS_PATH.'/Load.php';
+
+
+
+
